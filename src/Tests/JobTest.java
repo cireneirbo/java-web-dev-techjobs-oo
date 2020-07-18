cb45a81393ffa48717df996faa59bfaf3030d25a
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-07-17 23:21:32 -0400

    keep finding missing tasks, but now it is finished

--- src/Tests/JobTest.java
+++ src/Tests/JobTest.java
@@ -47,6 +47,22 @@ public class JobTest {
         Assert.assertNotEquals(job.getId(), job2.getId(),0.001);
     }
 
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job jobWithAllFields = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Assert.assertTrue(jobWithAllFields.getCoreCompetency() instanceof CoreCompetency);
+        Assert.assertTrue(jobWithAllFields.getLocation() instanceof Location);
+        Assert.assertTrue(jobWithAllFields.getEmployer() instanceof Employer);
+        Assert.assertTrue(jobWithAllFields.getPositionType() instanceof PositionType);
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job jobOne = new Job("my job", employer, location, positionType, coreCompetency);
+        Job jobTwo = new Job("my job", employer, location, positionType, coreCompetency);
+        Assert.assertNotEquals(jobOne.getId(), jobTwo.getId(), .001);
+    }
+
 
 
 }
